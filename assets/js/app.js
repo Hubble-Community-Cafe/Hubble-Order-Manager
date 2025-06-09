2525f07957065b391f585b8b76780252f32bc292
Victor Klomp <victor@gewis.nl>
2025-06-09 20:56:13 +0200

    feat: add console promo

--- assets/js/app.js
+++ assets/js/app.js
@@ -79,3 +79,37 @@ if (process.env.NODE_ENV === "development") {
   })
 }
 
+console.log(
+  `%c
+█████████ █████████  
+███    ▀▀ ███        
+ ███  ▄▄▄ ████████   
+ ███   ██ ███        
+  ███████ ███████    
+   ▄▄▄▄     ▄▄▄▄
+   ████  ▄  ████     
+   ▀███▄███▄███▀
+    █████▀█████      
+    ▀███▀ ▀███▀
+     ▀▀▀ ▀ ▀▀▀
+      ███████        
+       █████         
+       █████         
+        ███          
+        ▀▀▀         
+       █████        
+      ███▀▀██        
+       ▀▀█▄▄        
+      ██▄▄███        
+       █████         
+
+`,
+  'color: #d40000',
+);
+console.log(
+  "%cCurious what's behind the scenes? Check out this project at %chttps://github.com/gewis%c!",
+  'font-size: 20px',
+  'font-size: 18px; color: #d40000;',
+  'font-size: 18px',
+);
+
